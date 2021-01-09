a1f5ad9080106ffed6e55a7de6bb46f4c3a41c03
07-mc <67789575+07-mc@users.noreply.github.com>
2021-01-09 11:14:20 +0800

    Create minecraft-ogg.py

---
+++ minecraft-ogg.py
@@ -0,0 +1,8 @@
+try:
+    import os,json
+    mc_path = input("mc:")
+    json_file = mc_path + "\\indexes\\" + os.listdir(mc_path + "\\indexes")[0]
+    o = open(json_file)
+    file_dict = json.load(o)
+    o.close()
+    print(file_dict)
